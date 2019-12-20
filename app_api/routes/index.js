9525568a2546a463aea4ce1b01c9f4945464530d
Jamie Hewitt <jamie.hewitt15@gmail.com>
2019-12-20 15:01:31 +0300

    first commit, working fixit app

---
+++ app_api/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var ctrlFixes = require('../controllers/fix');
+var ctrlComments = require('../controllers/comment');
+
+// fixes
+router.get('/fix', ctrlFixes.fixesListByUpvotes);
+router.post('/fix', ctrlFixes.fixesCreate);
+router.get('/fix/:fixid', ctrlFixes.fixesReadOne);
+router.put('/fix/:fixid', ctrlFixes.fixesUpdateOne);
+router.post('/fix/:fixid', ctrlFixes.fixesUpvote);
+router.delete('/fix/:fixid', ctrlFixes.fixesDeleteOne);
+
+// comments
+router.post('/fix/:fixid/comment', ctrlComments.commentsCreate);
+router.get('/fix/:fixid/comment/:commentid', ctrlComments.commentsReadOne);
+router.put('/fix/:fixid/comment/:commentid', ctrlComments.commentsUpdateOne)
+router.delete('/fix/:fixid/comment/:commentid', ctrlComments.commentsDeleteOne);
+
+module.exports = router;
